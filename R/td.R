0662cf3bb2626fababcbe29f91db4227fc0e19a1
AQlT <alain.quartier@yahoo.fr>
2022-03-20 12:03:23 +0100

    td.f documentation

--- R/td.R
+++ R/td.R
@@ -29,29 +29,41 @@ NULL
 #' \mjsdeqn{
 #' \Delta_s\Delta y_t - \overline{\Delta_s \Delta y} =\beta \Delta_s \Delta TD_t +  \varepsilon_t
 #' }
+#' \item \code{model = "AIRLINE"} the following model is used:
+#' \mjsdeqn{
+#' y_t =\beta TD_t +  \varepsilon_t \text{ with }\varepsilon_t \sim ARIMA(0,1,1)(0,1,1)
+#' }
+#' \item \code{model = "R011"} the following model is used:
+#' \mjsdeqn{
+#' y_t =\beta TD_t +  \varepsilon_t \text{ with }\varepsilon_t \sim ARIMA(0,1,1)(0,1,1)
+#' }
+#' \item \code{model = "R100"} the following model is used:
+#' \mjsdeqn{
+#' y_t =\alpha_0 + \alpha_1 y_{t-1} + \beta TD_t +  \varepsilon_t
+#' }
 #' }
 #' @export
 #'
 #' @examples
 td.f<-function(s, model=c("D1", "DY", "DYD1", "WN", "AIRLINE", "R011", "R100"), nyears=0){
   model<-match.arg(model)
-  jts<-rjd3toolkit:::ts_r2jd(s)
+  jts<-rjd3toolkit::ts_r2jd(s)
   jtest<-.jcall("demetra/modelling/r/TradingDaysTests", "Ldemetra/stats/StatisticalTest;", "fTest",
                 jts, model, as.integer(nyears))
-  return (rjd3toolkit:::jd2r_test(jtest))
+  return (rjd3toolkit::jd2r_test(jtest))
 }
 
-#' Title
+#' Canova-Hansen Trading Days test
 #'
-#' @param s
-#' @param differencing
+#' @inheritParams td.f
+#' @param differencing differencing lags
 #'
 #' @return
 #' @export
 #'
 #' @examples
 td.ch<-function(s, differencing){
-  jts<-rjd3toolkit:::ts_r2jd(s)
+  jts<-rjd3toolkit::ts_r2jd(s)
   return (.jcall("demetra/modelling/r/TradingDaysTests", "[D", "chTest",
                 jts, .jarray(as.integer(differencing))))
 }
